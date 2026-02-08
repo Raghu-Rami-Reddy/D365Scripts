d9970a89172839af92d4f967e86f1bef6044901f
Raghu <141067105+Raghu-Rami-Reddy@users.noreply.github.com>
2026-02-08 10:12:27 +0530

    first creation

---
+++ test_script.js
@@ -0,0 +1,12 @@
+﻿//Download button
+
+//Custom Save button
+
+//onSave scenario: Disable Company field after record creation
+function lockCompanyOnUpdate(executionContext) {
+    var formContext = executionContext.getFormContext();
+
+    if (formContext.ui.getFormType() !== 1) {
+        formContext.getControl("rogcorp_company").setDisabled(true);
+    }
+}
